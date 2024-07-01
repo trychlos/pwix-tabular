2aafbf93476c4038ca5a118024073d704c277913
Pierre Wieser <p.wieser@trychlos.org>
2024-07-01 22:02:43 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:tabular',
-    version: '1.2.0',
+    version: '1.2.1-rc',
     summary: 'Extends aldeed:tabular package for our applications',
     git: 'https://github.com/trychlos/pwix-tabular.git',
     documentation: 'README.md'
